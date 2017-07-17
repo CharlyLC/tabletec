f5ffcdc89b87d565dc710bbcd820714d509dfedf
Juan Carlos Labrandero <charly@debian>
2017-07-17 01:58:33 -0400

    Prettify home page

--- src/client/views/home.jsx
+++ src/client/views/home.jsx
@@ -37,6 +37,10 @@ class Home extends Reflux.Component {
 		});
 	}
 
+	componentDidMount() {
+    	$('.parallax').parallax();
+    }
+
 	render() {
 		return (
 		<div className="rs-body">
@@ -48,8 +52,47 @@ class Home extends Reflux.Component {
 				<div className="center-align">
 					<img src="/images/tabletec-banner.png" alt="" className="responsive-img"/>
 				</div>
+
+				<section>
+					<div className="container">
+						<div className="section">
+							<div className="row">
+								<div className="col s12 center">
+									<h3><i className="mdi-content-send brown-text"></i></h3>
+									<h5 style={{ textShadow:' 1px 1px 1px #999'}}>NOSOTROS</h5>
+									<h6 className="center-align" >Somos una empresa dedicada a la comercialización de tableros melaminicos, aglomerados, multilaminados , MDF y todo insumos para la fabricación de muebles</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section>
+					<div className="parallax-container valign-wrapper">
+						<div className="parallax">
+							<img src="/images/tabletecImage1.jpg" alt=""/>
+						</div>
+					</div>
+				</section>
 			</main>
-			<Footer/>
+			
+			<footer className="page-footer red darken-4">
+				<div className="container">
+					<div className="row">
+						<div className="col s8 offset-s4">
+							<h5 className="white-text" style={{ textShadow:' 1px 1px 1px #000'}}>Contáctanos</h5>
+							<h6 className="grey-text text-lighten-4">Teléfono: 4 6441156</h6>
+							<h6 className="grey-text text-lighten-4">Dirección: Av. German Busch # 738 Ciudad Sucre, Chuquisaca, Bolivia</h6>
+							<h6 className="grey-text text-lighten-4">Messenger: @tabletecinsumosparaelmueble</h6>
+						</div>
+					</div>
+				</div>
+				<div className="footer-copyright">
+					<div className="container">
+						©2017 <a className="brown-text text-lighten-3" href="/">TableTec</a>. Todos los derechos reservados.
+					</div>
+				</div>
+			</footer>
 		</div>);
 	}
 }
